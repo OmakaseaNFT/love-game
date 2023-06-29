5301d88456808bcb0df47d64644af3ef6b102984
“morebug95” <ibkresnasudiatmika@gmail.com>
2023-06-30 00:05:50 +0800

    fix animation heart

--- components/heartbreaker/game.tsx
+++ components/heartbreaker/game.tsx
@@ -14,14 +14,14 @@ gsap.registerPlugin(TweenLite);
 const Game = () => {
   const [bgImage, setBgImage] = useState("/assets/screen-bg.jpg");
   const bgImage2 = "/assets/bg-loop.png";
-  const [isAnimating, setIsAnimating] = useState(true);
+  const [isAnimating, setIsAnimating] = useState(false);
   const bgRef = useRef(null);
   const heartRef: any = useRef(null);
   const tween: any = useRef(null);
   const heartTween: any = useRef(null);
-  const [dimensions, setDimensions] = useState({ width: 40, height: 40 });
-  const [heartImage, setHeartImage] = useState(HeartStatic);
-  const [bottomSpace, setBottomSpace] = useState(false);
+  const [switchImage, setSwitchImage] = useState(false);
+  const [crack, setCrack] = useState(false);
+  const [breakWidth, setBreakWidth] = useState({ width: 0, height: 0 });
   const {
     balance,
     mult,
@@ -55,106 +55,65 @@ const Game = () => {
         tween.current.restart();
       },
     });
-
-    heartTween.current = gsap.to(heartRef.current.firstChild, 18, {
-      css: {
-        height: "200px",
-        width: "200px",
-        transformOrigin: "50% 50%",
-        transform: "translateY(35px)",
-      },
-      onComplete: () => {
-        tween.current.restart();
-      },
-      paused: true,
-    });
   }, []);
 
-  const handleStartStopClick = () => {
-    if (isAnimating) {
-      startSound.current.play();
-      setHeartImage(Heart);
-      setBottomSpace(true);
-      heartTween.current.resume();
+  useEffect(() => {
+    if (switchImage && heartRef.current) {
+      heartTween.current = gsap.to(heartRef.current.firstChild, 18, {
+        css: {
+          height: "200px",
+          width: "200px",
+          transformOrigin: "50% 50%",
+          transform: `translateY(${35}px)`,
+        },
+        onComplete: () => {
+          tween.current.restart();
+        },
+        paused: false,
+      });
     }
+  }, [switchImage, crack]);
+
+  const handleStartStopClick = () => {
+    startSound.current.play();
+    setSwitchImage(true);
+    setIsAnimating(true);
   };
 
   const handleStop = () => {
-    let dimention;
-    let space;
-    if (isAnimating && bottomSpace) {
-      setDimensions({
-        width: heartRef.current.firstChild.offsetWidth,
-        height: heartRef.current.firstChild.offsetHeight,
-      });
-
-      console.log(heartRef.current.firstChild.offsetWidth);
-      if (
-        heartRef.current.firstChild.offsetWidth > 60 &&
-        heartRef.current.firstChild.offsetWidth < 140
-      ) {
-        dimention = 20;
-        space = 1;
-      } else if (heartRef.current.firstChild.offsetWidth > 140) {
-        dimention = 20;
-        space = 8;
-      } else {
-        dimention = 10;
-        space = -1;
-      }
-
-      heartRef.current.firstChild.style.width = `${
-        heartRef.current.firstChild.offsetWidth - dimention
-      }px`;
-      heartRef.current.firstChild.style.height = `${
-        heartRef.current.firstChild.offsetHeight - dimention
-      }px`;
-      if (heartRef.current.firstChild.offsetWidth > 140) {
-        heartRef.current.firstChild.style.transform = `translateY(${space}px)`;
-      } else {
-        heartRef.current.firstChild.style.transform = `translateY(${space}px)`;
-      }
-
-      setIsAnimating(false);
-      tween.current.pause();
-      heartTween.current.pause();
-      setBottomSpace(false);
-
+    if (isAnimating) {
       startSound.current.pause();
       startSound.current.currentTime = 0;
       stopSound.current.play();
+
+      if (heartRef.current) {
+        const imgElement = heartRef.current.firstChild;
+        const currentWidth = imgElement.offsetWidth;
+        const currentHeight = imgElement.offsetHeight;
+        setBreakWidth({ width: currentWidth, height: currentHeight });
+      }
+
+      tween.current.pause();
+      heartTween.current.pause();
+      setCrack(true);
+      setIsAnimating(false);
     }
   };
 
   const handleResetClick = () => {
-    setBottomSpace(false);
-    setIsAnimating(true);
-    setHeartImage(HeartStatic);
+    setSwitchImage(false);
+    setCrack(false);
     setBgImage("/assets/screen-bg.jpg");
     TweenLite.set(bgRef.current, {
       backgroundPosition: "0 100%",
     });
-
+    setIsAnimating(false);
     if (tween.current) {
       tween.current.progress(0).pause();
     }
-
-    TweenLite.set(heartRef.current, {
-      scale: 1,
-    });
-
-    TweenLite.set(heartRef.current.firstChild, {
-      css: {
-        height: "40px",
-        width: "40px",
-        transformOrigin: "50% 50%",
-      },
-    });
-
     if (heartTween.current) {
       heartTween.current.progress(0).pause();
     }
-
     startSound.current.pause();
     startSound.current.currentTime = 0;
   };
@@ -205,20 +164,37 @@ const Game = () => {
         )}
         <div
           ref={heartRef}
-          className={`absolute inset-0 flex items-end justify-center -ml-1 ${
-            bottomSpace ? "bottom-[48px]" : "bottom-[54px]"
-          }
-          }`}
+          className={`absolute inset-0 flex items-end justify-center -ml-1 
+          bottom-[53px]`}
         >
-          <Image
-            src={isAnimating ? heartImage : HeartBreakImage}
-            width={dimensions.width}
-            height={dimensions.height}
-            alt="heart"
-            className={`transition-opacity duration-500 ${
-              !isAnimating ? "bottom-[53px]" : ""
-            }`}
-          />
+          {!crack ? (
+            switchImage ? (
+              <div ref={heartRef}>
+                <Image
+                  src={Heart}
+                  width={500}
+                  height={500}
+                  alt="ntap"
+                  className="h-14 w-14 -mb-2"
+                />
+              </div>
+            ) : (
+              <Image
+                src={HeartStatic}
+                width={40}
+                height={40}
+                alt="ntap"
+                className="h-10 w-10"
+              />
+            )
+          ) : (
+            <Image
+              src={HeartBreakImage}
+              width={breakWidth.width - 20}
+              height={breakWidth.height - 20}
+              alt="ntap"
+            />
+          )}
         </div>
         <div className="flex flex-row absolute bottom-1 right-1 h-[20px] w-[50px]">
           <span className="text-white relative text-bold text-[1.3rem]">
