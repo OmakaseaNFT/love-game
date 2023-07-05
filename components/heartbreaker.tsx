880dff991da8fa1b4675d01a92cd28aa7b791817
Richard Forrester <richard.forrester@gmail.com>
2023-07-05 21:26:07 +0800

    add sig request for bets (#14)
    * add sig request for bets
    
    * update and remove log
    
    * add address for bet data

--- components/heartbreaker.tsx
+++ components/heartbreaker.tsx
@@ -1,22 +1,33 @@
+import { useEffect } from "react";
 import { HeartBreakerProvider } from "../system/context/HeartbreakerContext";
+import { useAuth } from "../system/hooks/useAuth";
 import Control from "./heartbreaker/control";
 import Features from "./heartbreaker/features";
 import Game from "./heartbreaker/game";
+import { AuthProvider } from "../system/context/AuthContext";
 
 export const HeartBreaker = () => {
+  const { onAuthUser } = useAuth();
+
+  useEffect(() => {
+    onAuthUser();
+  }, []);
+
   return (
-    <HeartBreakerProvider>
-      <div className="flex flex-col w-[100%] h-[90%]">
-        <div className="flex flex-1 w-[100%] w-[100%] my-[1rem] justify-between">
-          <div className="w-[255px] ">
-            <Game />
+    <AuthProvider>
+      <HeartBreakerProvider>
+        <div className="flex flex-col w-[100%] h-[90%]">
+          <div className="flex flex-1 w-[100%] w-[100%] my-[1rem] justify-between">
+            <div className="w-[255px] ">
+              <Game />
+            </div>
+            <Control />
           </div>
-          <Control />
+          <div className="flex-1 overflow-y-auto overflow-x-hidden">
+            <Features />
+          </div>
         </div>
-        <div className="flex-1 overflow-y-auto overflow-x-hidden">
-          <Features />
-        </div>
-      </div>
-    </HeartBreakerProvider>
+      </HeartBreakerProvider>
+    </AuthProvider>
   );
 };
