2ff1fadcc29c6cba23ce039c3b20bd958c801453
“morebug95” <ibkresnasudiatmika@gmail.com>
2023-08-14 13:53:28 +0800

    add love grant

--- components/paper.tsx
+++ components/paper.tsx
@@ -1,121 +1,158 @@
+import { ethers } from "ethers";
+import TestingABI from "../utils/testing.json";
+import LoveNFTABI from "../utils/loveNFT.json";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const loveNFTAddress = "0x570322Cd5Ee9A4EC341Bc85233C062cBA4724de0";
+const loveTokenAddress = "0x2aA7b05CaDCb2A6604544131D556d68b5c18635e";
 const Paper = () => {
-  const menuBars = [
-    {
-      text: "<u>F</u>ile",
-    },
-    {
-      text: "<u>E</u>dit",
-    },
-    {
-      text: "<u>V</u>iew",
-    },
-    {
-      text: "<u>H</u>elp",
-    },
-  ];
+  const [multiply, setMultiply] = useState(0);
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    setMultiply(value * 1000);
+  }, [value]);
+  function getTimestampInSeconds() {
+    return Math.floor(Date.now() / 1000);
+  }
+
+  const handleMint = async () => {
+    const provider = new ethers.providers.Web3Provider(
+      (window as any).ethereum
+    );
+    const signer = provider.getSigner();
+
+    const chainId = (await provider.getNetwork()).chainId;
+
+    const loveNFTContract = new ethers.Contract(
+      loveNFTAddress,
+      LoveNFTABI,
+      provider
+    ) as any;
+
+    const myTokenContract = new ethers.Contract(
+      loveTokenAddress,
+      TestingABI,
+      provider
+    ) as any;
+
+    const getAddressSigner = await signer.getAddress();
+    const myTokenBalance = await myTokenContract.balanceOf(getAddressSigner);
+
+    const nonces = await myTokenContract.nonces(getAddressSigner);
+
+    const domain = {
+      name: await myTokenContract.name(),
+      version: "1",
+      chainId: chainId,
+      verifyingContract: myTokenContract.address,
+    };
+    console.log(domain);
+
+    const types = {
+      Permit: [
+        {
+          name: "owner",
+          type: "address",
+        },
+        {
+          name: "spender",
+          type: "address",
+        },
+        {
+          name: "value",
+          type: "uint256",
+        },
+        {
+          name: "nonce",
+          type: "uint256",
+        },
+        {
+          name: "deadline",
+          type: "uint256",
+        },
+      ],
+    };
+
+    const valueData = ethers.utils.parseEther(multiply.toString());
+    const deadline = getTimestampInSeconds() + 4200;
+
+    const values = {
+      owner: getAddressSigner,
+      spender: "0x570322Cd5Ee9A4EC341Bc85233C062cBA4724de0",
+      value: valueData,
+      nonce: nonces,
+      deadline: deadline,
+    };
+
+    // Sign the typed data using the signer
+    const signature = await signer._signTypedData(domain, types, values);
+
+    // split the signature into its components
+    const sig = ethers.utils.splitSignature(signature);
+
+    const loveNFTContractWithSigner = loveNFTContract.connect(signer);
+
+    // verify the Permit type data with the signature
+    const recovered = ethers.utils.verifyTypedData(domain, types, values, sig);
+
+    // get network gas price
+    const gasPrice = await provider.getGasPrice();
+
+    console.log(gasPrice.toString(), sig.v, sig.r, sig.s);
+
+    console.log(loveNFTContract);
+
+    let estimatedGas = await loveNFTContractWithSigner.estimateGas.mint(
+      ethers.utils.parseEther(multiply.toString()),
+      deadline,
+      sig.v,
+      sig.r,
+      sig.s
+    );
+
+    const buffer = estimatedGas.mul(120).div(100); // adding 20% buffer
+    const gasLimit = estimatedGas.add(buffer);
+
+    let tx = await loveNFTContractWithSigner.mint(
+      ethers.utils.parseEther(multiply.toString()),
+      deadline,
+      sig.v,
+      sig.r,
+      sig.s,
+      {
+        gasPrice: gasPrice,
+        gasLimit: gasLimit,
+        //hardcoded gas limit; change if needed
+      }
+    );
+
+    await tx.wait(2);
+  };
+
   return (
-    <div className="w-full flex flex-col">
-      <div className="text-gray-500 mt-1">
-        {menuBars.map((item, idx) => (
-          <span
-            key={idx}
-            dangerouslySetInnerHTML={{ __html: item.text }}
-            className="mr-2"
-          />
-        ))}
+    <div className="flex flex-row items-center mt-[40px]">
+      <div className="mr-2 w-2/3">
+        <Image alt="" src="/assets/lovenft.png" width={2000} height={2000} />
       </div>
-      <div className="border-l-gray-600 border-t-gray-600 border-r-gray-200 border-b-gray-200 border-[1.6px] bg-white w-[100%] flex aspect-[1.15] m-auto mt-1 py-1 px-2 overflow-auto">
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <h2 style={{fontWeight: "bold"}}>Launch Timeline & Overview</h2>
-          <ul style={{listStyleType: "circle", marginLeft: "15px"}}>
-            <li>$LOVE smart contract will be on Ethereum Mainnet</li>
-            <li>
-              Community will review contract code for transparency and safety
-            </li>
-            <li>
-              Transactions via love.game UI enabled after code review period
-            </li>
-            <li>Eligible wallet holders can claim $LOVE on love.game UI</li>
-            <li>
-              To get ≥1 $LOVE token: purchase or obtain from existing holder
-            </li>
-            <li>Revenue mechanisms managed by $LOVE token holders</li>
-            <li>
-              Token liquidity supported by holders through yield incentives on
-              bonding curve
-            </li>
-          </ul>
-
-          <h2 style={{fontWeight: "bold"}}>Purchasing $LOVE Tokens</h2>
-
-          <ul style={{listStyleType: "circle", marginLeft: "15px"}}>
-            <li>
-              {`The $LOVE smart contract will be deployed on the Ethereum Mainnet.
-              To ensure transparency and safety, we'll provide a few hours for
-              the community to review the contract code. Only after this review
-              period, we'll enable transactions to occur through the love.game
-              user interface (UI).`}
-            </li>
-            <li>
-              Once transactions are allowed, those with eligible wallets can
-              claim $LOVE tokens via the love.game UI.
-            </li>
-            <li>
-              {`However, there's a prerequisite.`}
-              <strong>
-                To conduct any transaction or to interact with the $LOVE
-                contract, you must already own at least 1 $LOVE token in your
-                wallet.
-              </strong>{" "}
-              {`Therefore, if you wish to acquire $LOVE tokens and didn't qualify
-              for an airdrop, you'll need to obtain 1 $LOVE token from an
-              existing $LOVE holder.`}
-            </li>
-            <li>
-              The entire revenue mechanisms are managed by $LOVE token holders.
-              These holders also support token liquidity by receiving yield
-              incentives on a bonding curve.
-            </li>
-          </ul>
-
-          <h2 style={{fontWeight: "bold"}}>Claiming $LOVE Tokens</h2>
-
-          <ul style={{listStyleType: "circle", marginLeft: "15px"}}>
-            <li>
-              Once the love.game UI is active, wallets that completed the 0ETH
-              transaction before the snapshot can claim their $LOVE tokens.
-            </li>
-            <li>This claiming opportunity will be available for a week.</li>
-          </ul>
-
-          <h2 style={{fontWeight: "bold"}}>Staking $LOVE Tokens</h2>
-
-          <ul style={{listStyleType: "circle", marginLeft: "15px"}}>
-            <li>
-              $LOVE token holders can supply liquidity to LOVE/ETH farms and
-              earn $LOVE emissions.
-            </li>
-            <li>
-              A single-sided staking pool will be introduced in the future.
-            </li>
-            <li>
-              Additionally, other farming pairs will be made available over
-              time.
-            </li>
-          </ul>
-
-          <h2 style={{fontWeight: "bold"}}>Upcoming Features & Utility</h2>
-
-          <ul style={{listStyleType: "circle", marginLeft: "15px"}}>
-            <li>
-              LIPS Governance portal: This will facilitate $LOVE Improvement
-              Proposals.
-            </li>
-            <li>
-              AI Compute: You will be able to use your $LOVE tokens to buy AI
-              compute power from the OmakaseaDev supercomputer.
-            </li>
-          </ul>
+      <div className="w-1/3">
+        <div className="text-[20px]">COLLECTION NAME</div>
+        <div className="text-[12px]">@beauti_100</div>
+        <div className="text-[16px]">1000 Love</div>
+        <div className="text-base">Amount to mint</div>
+        <div>
+          <input
+            type="number"
+            className="w-full"
+            value={value}
+            onChange={(e) => setValue(parseInt(e.target.value))}
+          />
+        </div>
+        <div className="text-base">Total $LOVE to mint</div>
+        <div className="text-[16px]">{multiply} Love</div>
+        <div className="btn" onClick={() => handleMint()}>
+          Mint Now
         </div>
       </div>
     </div>
