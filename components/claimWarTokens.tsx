affd2c3f6931605fe54d185bfc4e090e5d335689
MrTuxedo <dev@based.consulting>
2023-09-06 17:36:37 -0700

    add TransactionNotificationWrapper to war claim

--- components/claimWarTokens.tsx
+++ components/claimWarTokens.tsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
 import axios from "axios";
-import { providers } from "ethers";
+import { useState } from "react";
+import { ContractTransaction, providers } from "ethers";
 import { useSignMessage } from "wagmi";
 
 import { AppContracts } from "../system/AppContracts";
 import ClaimIcon from "../assets/claim.png";
+
+import {
+  requestErrorState,
+  requestPendingState,
+  requestSuccessState,
+  useRequestState,
+} from "../system/hooks/useRequestState";
+import { TransactionNotificationWrapper } from "./ui/TransactionNotificationWrapper";
+
+const rickRoll = () =>
+  window.open(
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    '_blank',
+    'noopener,noreferrer'
+    );
+
 
 const ClaimWarTokens = () => {
   const provider = new providers.Web3Provider((window as any).ethereum);
@@ -12,48 +29,76 @@ const ClaimWarTokens = () => {
   const { warClaimContract } = new AppContracts(signer);
     
   const { signMessageAsync } = useSignMessage();
+  const { requestState, setRequestState } = useRequestState();
+  const [ errorMessage, setErrorMessage ] = useState<string>("");
 
-  const rickRoll = () =>
-    window.open(
-      "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-      '_blank',
-      'noopener,noreferrer'
-      );
-  
+
+  const handleTransactionSuccess = () =>
+    setRequestState(requestSuccessState);
+
+  const handleTransactionError = (error: any) => {
+    setErrorMessage(error?.reason);
+    setRequestState(requestErrorState);
+  }
+
+  const rickRollError = (error: any) => {
+    rickRoll();
+    handleTransactionError(error);
+  }
+
   const claim = async () => {
+    setRequestState(requestPendingState);
     const message = `ALL IS FAIR IN $LOVE AND $WAR`;
+    
+    let sig = "";
 
-    const sig = await signMessageAsync({message});
-
+    try {
+      sig = await signMessageAsync({message});
+    } catch (error: any) {
+      return handleTransactionError({ reason: `Failed to create signature with wallet!`});
+    }
+  
     const result = await axios.post(`/api/claimTokens`, {
       signature: sig,
       message
     });
-
+    
     const owner = await signer.getAddress();
     const hasClaimed = await warClaimContract.hasClaimed(owner);
-
-    if (!result.data || hasClaimed) return rickRoll();
-
+    
+    if (!result.data) {
+      return rickRollError({ reason: `Address is not eligible for claim!` });
+    } else if (hasClaimed) {
+      return rickRollError({ reason: `Address has already claimed!` })
+    }
+    
     const { messageHash, signature } = result.data;
 
     try {
-      return await warClaimContract.claim(messageHash, signature)
-    } catch (e) {
-      return rickRoll();
+      const tx = await warClaimContract.claim(messageHash, signature) as ContractTransaction;
+      await tx.wait(2)
+      handleTransactionSuccess();
+    } catch (error: any) {
+      rickRollError(error);
     }
   };
 
   return (
-    <div className="flex flex-col gap-1 justify-center items-center mt-4 mx-auto">
-      <Image src={ClaimIcon} alt="" width={48} height={48} />
-      <button
-        onClick={async () => await claim()}
-        className="btn w-auto h-[35px] m-auto my-2"
-      >
-        Claim $WAR!
-      </button>      
-    </div>
+    <TransactionNotificationWrapper
+      requestState={requestState}
+      setRequestState={setRequestState}
+      errorMessage={errorMessage}
+    >
+      <div className="flex flex-col gap-1 justify-center items-center mt-4 mx-auto">
+        <Image src={ClaimIcon} alt="" width={48} height={48} />
+        <button
+          onClick={async () => await claim()}
+          className="btn w-auto h-[35px] m-auto my-2"
+        >
+          Claim $WAR!
+        </button>      
+      </div>
+    </TransactionNotificationWrapper>
   );
 };
 
