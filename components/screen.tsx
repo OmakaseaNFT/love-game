7c52cd3e80fd70e9a6cf05db26704272bbe801a3
MrTuxedo <dev@based.consulting>
2023-09-07 14:31:59 -0700

    add blank background

--- components/screen.tsx
+++ components/screen.tsx
@@ -130,7 +130,7 @@ const Screen = ({
                   >
                     <Image src={item.icon} alt="icon" height={55} />
                     {item.label && (
-                    <div className="text-[rgba(255,255,255,.7)] text-lg mt-[2px]">
+                    <div className="text-[rgba(255,255,255,.80)] text-lg mt-2">
                       {item.label}
                     </div>
                   )}
