e6f12b96f4ddd236f723d8ee9e3cf3636248e29b
MrTuxedo <dev@based.consulting>
2023-09-15 15:01:05 -0700

    add discord to screen

--- utils/constant.ts
+++ utils/constant.ts
@@ -11,27 +11,32 @@ export const contractAddressWar: string =
   process.env.NEXT_PUBLIC_CONTRACT_WAR || "";
 export const contractAddressHeartbreak =
   process.env.NEXT_PUBLIC_CONTRACT_HEARTBREAKER!;
-export const ETHLOVEPoolAddy: string = "0xdd97fce8441dabf221b330269750b18ba82b0cd6";
-export const USDTLOVEPoolAddy: string = "0xE24Ab719209A9844E59dBfEEe91ce7d8D482532e";
-export const WARLOVEPoolAddy: string = "0x1c9189593669b5c1fbc33d7ff522b1b2dd0646e4";
-export const contractAddressUsdt: string = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-export const contractAddressWbtc: string = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"
-export const contractAddressPepe: string = "0x6982508145454ce325ddbe47a25d4ec3d2311933";
-export const contractAddressBobo: string = "0xb90b2a35c65dbc466b04240097ca756ad2005295";
-export const contractAddressMog: string = "0xaaee1a9723aadb7afa2810263653a34ba2c21c7a";
-export const contractAddressNina: string = "0x697a79AF2De4Af9E9aA0D08905374556AD1353bB";
-export const ETHERSCAN_LOVE_LINK =
-  `https://etherscan.io/address/${contractAddressLove}`;
-export const ETHERSCAN_FARM_LINK =
-  `https://etherscan.io/address/${contractAddressLoveFarm}`;
+export const ETHLOVEPoolAddy: string =
+  "0xdd97fce8441dabf221b330269750b18ba82b0cd6";
+export const USDTLOVEPoolAddy: string =
+  "0xE24Ab719209A9844E59dBfEEe91ce7d8D482532e";
+export const WARLOVEPoolAddy: string =
+  "0x1c9189593669b5c1fbc33d7ff522b1b2dd0646e4";
+export const contractAddressUsdt: string =
+  "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+export const contractAddressWbtc: string =
+  "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599";
+export const contractAddressPepe: string =
+  "0x6982508145454ce325ddbe47a25d4ec3d2311933";
+export const contractAddressBobo: string =
+  "0xb90b2a35c65dbc466b04240097ca756ad2005295";
+export const contractAddressMog: string =
+  "0xaaee1a9723aadb7afa2810263653a34ba2c21c7a";
+export const contractAddressNina: string =
+  "0x697a79AF2De4Af9E9aA0D08905374556AD1353bB";
+export const ETHERSCAN_LOVE_LINK = `https://etherscan.io/address/${contractAddressLove}`;
+export const ETHERSCAN_FARM_LINK = `https://etherscan.io/address/${contractAddressLoveFarm}`;
 
 export const UNISWAP_LINK = `https://app.uniswap.org/#/add/v2/${contractAddressLove}/ETH`;
-export const BIT_QUERY_LINK =
-  `https://explorer.bitquery.io/ethereum/token/${contractAddressLove}`;
-export const DEXSCREENER_LINK =
-  `https://dexscreener.com/ethereum/${ETHLOVEPoolAddy}`;
+export const BIT_QUERY_LINK = `https://explorer.bitquery.io/ethereum/token/${contractAddressLove}`;
+export const DEXSCREENER_LINK = `https://dexscreener.com/ethereum/${ETHLOVEPoolAddy}`;
 export const LOVE_ETH_LP_LINK = `https://app.uniswap.org/#/add/v2/ETH/${contractAddressLove}`;
-export const WAR_SWAP_LINK = `https://app.uniswap.org/#/swap?theme=dark&inputCurrency=${contractAddressLove}&outputCurrency=${contractAddressWar}`;
+export const WAR3_SWAP_LINK = `https://app.uniswap.org/#/swap?theme=dark&inputCurrency=${contractAddressLove}&outputCurrency=${contractAddressWar}`;
 
 export const ETHERSCAN_FAITH_LINK = `https://etherscan.io/address/${contractAddressFaith}`;
 
@@ -40,6 +45,7 @@ export const HOT_JAR_SITE_ID = 3527265;
 export const HOT_JAR_VERSION = 6;
 export const TELEGRAM_LINK = "https://t.me/loveethereum";
 export const TWITTER_LINK = "https://twitter.com/lovegameeth";
+export const WAR3_TWITTER_LINK = "https://twitter.com/webwar3";
 export const DISCORD_LINK = "https://discord.gg/loveethereum";
 
 export const IS_DEV_ENV = process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true";
@@ -50,7 +56,7 @@ export const KV_REST_API_URL = IS_DEV_ENV
   : process.env.KV_PROD_REST_API_URL || "";
 
 export const KV_REST_API_TOKEN = IS_DEV_ENV
-  ? process.env.KV_REST_API_TOKEN || "" 
+  ? process.env.KV_REST_API_TOKEN || ""
   : process.env.KV_PROD_REST_API_TOKEN || "";
 
 export const LOVE_POOLS = [
